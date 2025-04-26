e4f3e8045fe317cf554d2e2b167ba50f7d06b898
nima-232 <nikatkebuchava03@gmail.com>
2025-04-26 01:30:40 -0700

    commit

---
+++ Level 00/homework/task01-5.py
@@ -0,0 +1,15 @@
+name = "nika"
+
+favorite_color1 = "black"
+favorite_color2 = "white"
+
+my_age = "16"
+friend1_age = "15"
+friend2_age = "22"
+
+mather_name = "xtia"
+father_name = "irakli"
+sister_name = "nia"
+
+have_pet = "no pet"
+have_bird = "drowned"
